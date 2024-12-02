a89d308898949b13789f8c1335ec7c2c1ee3096c
Akanksha <ayushiaku7400@gmail.com>
2024-12-02 15:30:05 +0530

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route  exact path="/" element={<Home/>} />
+        <Route  exact path="/login" element={<Login/>} />
+        <Route  exact path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   )
